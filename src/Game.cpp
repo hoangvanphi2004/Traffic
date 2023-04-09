190f68fe7561b1c2a30b8272c3c1b70f2e7df160
hoangvanphi2004 <hoangvanphi2004@gmail.com>
2023-04-09 23:32:48 +0700

    Game Demo1

--- src/Game.cpp
+++ src/Game.cpp
@@ -24,7 +24,7 @@ void Game::runningScreen(){
 Game::Game(const char *title, int screen_width, int screen_height){
     SDL_Init(SDL_INIT_VIDEO);
 
-    window = SDL_CreateWindow("Go Home", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screen_width, screen_height, SDL_WINDOW_SHOWN);
+    window = SDL_CreateWindow("Traffic", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screen_width, screen_height, SDL_WINDOW_SHOWN);
     materials->renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
     materials->loadMaterials();
--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "../Include/def.h"
 
 int main(int argc, char *args[]){
-    Game* game = new Game("GoHome", SCREEN_WIDTH, SCREEN_HEIGHT);
+    Game* game = new Game("Traffic", SCREEN_WIDTH, SCREEN_HEIGHT);
     //game.menuScreen();
     game->runningScreen();
     return 0;
